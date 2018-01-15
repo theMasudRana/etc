8f549485a49af49eb9037a02f33c4cff08abdf36
theMasudRana <mr.masudrana00@gmail.com>
2018-01-15 13:52:39 +0600

    Resposive Ok

--- cryptox/js/plugins.js
+++ cryptox/js/plugins.js
@@ -236,35 +236,30 @@ function hexToRgb(e){var a=/^#?([a-f\d])([a-f\d])([a-f\d])$/i;e=e.replace(a,func
 
     // mailchimp start
     if ($('.subscription-form').length > 0) {
-        /*  MAILCHIMP  */
         $('.subscription-form').ajaxChimp({
-            language: 'en',
             callback: mailchimpCallback,
             url: "https://masudrana.us14.list-manage.com/subscribe/post?u=bbaf30c9498959598e94abea6&amp;id=ea892eba33" 
             //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".
         });
+        function mailchimpCallback(resp) {
+            if (resp.result === 'success') {
+                $('.subscription-success').html('<i class="fa fa-check"></i><br/>' + resp.msg).fadeIn(1000);
+                $('.subscription-error').fadeOut(500);
+            } else if (resp.result === 'error') {
+                $('.subscription-error').html('<i class="fa fa-times"></i><br/>' + resp.msg).fadeIn(1000);
+            }
+        }        
     }
-
-    function mailchimpCallback(resp) {
-        if (resp.result === 'success') {
-            $('.subscription-success').html('<i class="fa fa-check"></i><br/>' + resp.msg).fadeIn(1000);
-            $('.subscription-error').fadeOut(500);
-        } else if (resp.result === 'error') {
-            $('.subscription-error').html('<i class="fa fa-times"></i><br/>' + resp.msg).fadeIn(1000);           
-        }
-    }
-    $.ajaxChimp.translations.en = {
-        'submit': 'Submitting...',
-        0: 'We have sent you a confirmation email',
-        1: 'Please enter a value',
-        2: 'An email address must contain a single @',
-        3: 'The domain portion of the email address is invalid (the portion after the @: )',
-        4: 'The username portion of the email address is invalid (the portion before the @: )',
-        5: 'This email address looks fake or invalid. Please enter a real email address'
-    };
-
 
 })(jQuery);
+
+/* ---------------------------------------------
+  jQuery Site Preloaded animation
+ --------------------------------------------- */
+$(window).on('load', function() {
+   $("#xboot-loader").fadeOut(500);
+});
+
 
 
 
